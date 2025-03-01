1489757b48135c0522bb19c11eb4a9db213fa465
haeyun-choi <haeyoon114@naver.com>
2025-03-01 18:29:11 +0000

    Deploying to gh-pages from @ haeyun-choi/haeyun-choi.github.io@7287d5c63989cafc3c61b6b938aee4260fa27242 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,16 +429,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-a-paper-about-deblurring-radiance-fields-is-accepted-to-cvpr-2025",
+          title: 'A paper about deblurring radiance fields is accepted to CVPR 2025.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
